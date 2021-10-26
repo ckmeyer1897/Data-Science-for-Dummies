8ba980d8e98cabc88d9e13fdbc981b3eeeb1e0c5
ckmeyer1897 <84290148+ckmeyer1897@users.noreply.github.com>
2021-10-26 03:20:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Custom imports 
 from multipage import MultiPage
-from pages import LoadData, DataUnderstanding, utils, data_viz, dataprep
+from pages import LoadData, DataUnderstanding, utils, data_viz, dataprep, modeling
 
 # Create an instance of the app 
 app = MultiPage()
@@ -24,22 +24,12 @@ with row0_2:
     st.subheader('Streamlit App by [Christian Meyer](https://www.linkedin.com/in/christian-meyer-1a0b21116/)')
 row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 
- #st.image(display, width = 400)
-# st.title("Data Storyteller Application")
-#col1, col2 = st.columns(2)
-#col1.image(display, width = 400)
-st.sidebar.title("1 - Data Understanding")#
-# with st.sidebar.beta_expander(app.add_page('Data Collection', LoadData.app)
 # Add all your application here
 app.add_page("Load Data", LoadData.app)
-app.add_page("Data Exploration", DataUnderstanding.app)
-app.add_page("Data Vizualization", data_viz.app)
+app.add_page("Data Exploration & Vizualization", DataUnderstanding.app)
+#app.add_page("Data Vizualization", data_viz.app)
 app.add_page("Data Preparation", dataprep.app)
-
-
-# st.sidebar.title("2 - Data Preparation")
-# st.sidebar.title("3 - Modeling")
-# st.sidebar.title("4 - Evaluation")
+app.add_page("Modeling", modeling.app)
 
 
 
