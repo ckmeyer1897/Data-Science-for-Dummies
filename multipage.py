322de0f5d7cf794f8ec3d2ae0e3ecbcf000af584
ckmeyer1897 <84290148+ckmeyer1897@users.noreply.github.com>
2021-10-26 05:59:23 -0400

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -32,6 +32,7 @@ class MultiPage:
     def run(self):
         # Drodown to select the page to run  
        # page = st.sidebar.selectbox(
+        st.title(page)
         page = st.sidebar.radio(
             ' ', 
             self.pages, 
